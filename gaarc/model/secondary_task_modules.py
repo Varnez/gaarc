b3154a7199cbc028016921c0a84cd11ce4944219
Varnez <varnez.ai@gmail.com>
2024-08-30 01:55:18 +0100

    Added a few more STMs related to size, entities' color and super entitie's center of mass.

--- gaarc/model/secondary_task_modules.py
+++ gaarc/model/secondary_task_modules.py
@@ -185,6 +185,273 @@ class EntityMassCentre(EntitySTM):
         return features
 
 
+class SuperEntityMassCentre(SuperEntitySTM):
+    """
+    Secondary task that will aim to predict the center of mass of a random super entity within
+    the provided sample.
+    """
+
+    _name: str = "Super Entity's Mass Center"
+    _loss_function: Loss = MSELoss()  # type: ignore[assignment]
+
+    def __init__(
+        self,
+        latent_space_size: int,
+        hidden_layer_size: int,
+        task_loss_weight: float = 1.0,
+        use_batch_norm: bool = True,
+    ):
+        super().__init__(
+            task_loss_weight=task_loss_weight,
+        )
+
+        self._x_classifier: nn.Module
+        self._y_classifier: nn.Module
+
+        if use_batch_norm:
+            self._x_classifier = Sequential(
+                BatchNorm1d(latent_space_size),
+                Linear(latent_space_size, hidden_layer_size),
+                BatchNorm1d(hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+            self._y_classifier = Sequential(
+                BatchNorm1d(latent_space_size),
+                Linear(latent_space_size, hidden_layer_size),
+                BatchNorm1d(hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+        else:
+            self._x_classifier = Sequential(
+                Linear(latent_space_size, hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+            self._y_classifier = Sequential(
+                Linear(latent_space_size, hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+    def get_input_features(self, sample: ARCSample, idx: int) -> np.ndarray:
+        super_entity = sample.super_entities[idx]
+
+        input_features = np.array(super_entity.entity)
+
+        input_features = np.expand_dims(input_features, axis=0)
+
+        return input_features
+
+    def get_target(self, sample: ARCSample, idx: int) -> np.ndarray:
+        super_entity = sample.super_entities[idx]
+
+        target = np.array(super_entity.center_of_mass)
+
+        return target
+
+    def forward(self, input_features: torch.Tensor, encoder: nn.Module) -> torch.Tensor:
+        features = super().forward(input_features, encoder)
+
+        x_features = self._x_classifier(features)
+        y_features = self._y_classifier(features)
+
+        features = torch.cat((x_features, y_features), dim=1)
+
+        return features
+
+
+class EntitySize(EntitySTM):
+    """
+    Secondary task that will aim to predict the amount of pixels of a random entity within
+    the provided sample.
+    """
+
+    _name: str = "Entity's Size"
+    _loss_function: Loss = MSELoss()  # type: ignore[assignment]
+
+    def __init__(
+        self,
+        latent_space_size: int,
+        hidden_layer_size: int,
+        task_loss_weight: float = 1.0,
+        use_batch_norm: bool = True,
+    ):
+        super().__init__(
+            task_loss_weight=task_loss_weight,
+        )
+
+        self._classifier: nn.Module
+
+        if use_batch_norm:
+            self._classifier = Sequential(
+                BatchNorm1d(latent_space_size),
+                Linear(latent_space_size, hidden_layer_size),
+                BatchNorm1d(hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+        else:
+            self._classifier = Sequential(
+                Linear(latent_space_size, hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+    def get_input_features(self, sample: ARCSample, idx: int) -> np.ndarray:
+        entity = sample.entities[idx]
+
+        input_features = np.array(
+            entity.entity,
+        )
+
+        input_features = np.expand_dims(input_features, axis=0)
+
+        return input_features
+
+    def get_target(self, sample: ARCSample, idx: int) -> np.ndarray:
+        super_entity = sample.entities[idx]
+
+        target = np.array(
+            super_entity.size,
+        )
+
+        target = np.expand_dims(target, axis=0)
+
+        return target
+
+    def forward(self, input_features: torch.Tensor, encoder: nn.Module) -> torch.Tensor:
+        features = super().forward(input_features, encoder)
+
+        features = self._classifier(features)
+
+        return features
+
+
+class SuperEntitySize(SuperEntitySTM):
+    """
+    Secondary task that will aim to predict the amount of pixels of a random super entity within
+    the provided sample.
+    """
+
+    _name: str = "Super Entity's Size"
+    _loss_function: Loss = MSELoss()  # type: ignore[assignment]
+
+    def __init__(
+        self,
+        latent_space_size: int,
+        hidden_layer_size: int,
+        task_loss_weight: float = 1.0,
+        use_batch_norm: bool = True,
+    ):
+        super().__init__(
+            task_loss_weight=task_loss_weight,
+        )
+
+        self._classifier: nn.Module
+
+        if use_batch_norm:
+            self._classifier = Sequential(
+                BatchNorm1d(latent_space_size),
+                Linear(latent_space_size, hidden_layer_size),
+                BatchNorm1d(hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+        else:
+            self._classifier = Sequential(
+                Linear(latent_space_size, hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+    def get_input_features(self, sample: ARCSample, idx: int) -> np.ndarray:
+        super_entities = sample.super_entities[idx]
+
+        input_features = np.array(
+            super_entities.entity,
+        )
+
+        input_features = np.expand_dims(input_features, axis=0)
+
+        return input_features
+
+    def get_target(self, sample: ARCSample, idx: int) -> np.ndarray:
+        super_entity = sample.super_entities[idx]
+
+        target = np.array(
+            super_entity.size,
+        )
+
+        target = np.expand_dims(target, axis=0)
+
+        return target
+
+    def forward(self, input_features: torch.Tensor, encoder: nn.Module) -> torch.Tensor:
+        features = super().forward(input_features, encoder)
+
+        features = self._classifier(features)
+
+        return features
+
+
+class EntityColor(EntitySTM):
+    _name: str = "Entity's Color"
+    _loss_function: Loss = MSELoss()  # type: ignore[assignment]
+
+    def __init__(
+        self,
+        latent_space_size: int,
+        hidden_layer_size: int,
+        task_loss_weight: float = 1.0,
+        use_batch_norm: bool = True,
+    ):
+        super().__init__(
+            task_loss_weight=task_loss_weight,
+        )
+
+        self._classifier: nn.Module
+
+        if use_batch_norm:
+            self._classifier = Sequential(
+                BatchNorm1d(latent_space_size),
+                Linear(latent_space_size, hidden_layer_size),
+                BatchNorm1d(hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+        else:
+            self._classifier = Sequential(
+                Linear(latent_space_size, hidden_layer_size),
+                Linear(hidden_layer_size, 1),
+            )
+
+    def get_input_features(self, sample: ARCSample, idx: int) -> np.ndarray:
+        entity = sample.entities[idx]
+
+        input_features = np.array(
+            entity.entity,
+        )
+
+        input_features = np.expand_dims(input_features, axis=0)
+
+        return input_features
+
+    def get_target(self, sample: ARCSample, idx: int) -> np.ndarray:
+        entity = sample.entities[idx]
+
+        target = np.array(
+            entity.color / ARC_ENTITY_UNIQUE_COLORS,
+        )
+
+        target = np.expand_dims(target, axis=0)
+
+        return target
+
+    def forward(self, input_features: torch.Tensor, encoder: nn.Module) -> torch.Tensor:
+        features = super().forward(input_features, encoder)
+
+        features = self._classifier(features)
+
+        return features
+
+
 class SuperEntityColors(SuperEntitySTM):
     _name: str = "Super Entity's Colors"
     _loss_function: Loss = MSELoss()  # type: ignore[assignment]
